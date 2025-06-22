5522b3be8edeb5055226d9127a9ba88372b5148f
Jonze123 <Balenor321@gmail.com>
2025-06-22 12:55:44 +0300

    Use Robert Jenkins 32 bit hash

--- assets/shaders/Builtin.ObjectShader.meshlet.mesh.glsl
+++ assets/shaders/Builtin.ObjectShader.meshlet.mesh.glsl
@@ -59,15 +59,16 @@ vec3 renormalize_normal(vec3 n)
    return n/a + b;
 }
 
-uint hash_index(uint k)
+uint32_t hash_index(uint32_t a)
 {
-   uint hash = 2166136261u;
+   a = (a+0x7ed55d16) + (a<<12);
+   a = (a^0xc761c23c) ^ (a>>19);
+   a = (a+0x165667b1) + (a<<5);
+   a = (a+0xd3a2646c) ^ (a<<9);
+   a = (a+0xfd7046c5) + (a<<3);
+   a = (a^0xb55a4f09) ^ (a>>16);
 
-   uint bits = k;
-   hash ^= bits;
-   hash *= 16777619u;
-
-   return hash;
+   return a;
 }
 
 void main()
