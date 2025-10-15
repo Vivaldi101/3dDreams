b1e0e61d9dc0e4f1b183c2f1dc894783a9fff23d
Jonze123 <Balenor321@gmail.com>
2025-10-15 16:20:16 +0300

    Fix build.bat

--- code/build.bat
+++ code/build.bat
@@ -2,49 +2,60 @@
 
 REM Check for argument
 IF "%1"=="" (
-    echo "Usage: %0 debug|release"
+    echo "Usage: %0 d|r"
     exit /b 1
 )
 
+REM Root of project (folder containing this batch file)
+SET ROOT=%~dp0
+
+REM -----------------------------
+REM Project paths and libraries
+REM -----------------------------
 set WIN32_LIBS=kernel32.lib user32.lib gdi32.lib
 set VULKAN_LIB=vulkan-1.lib
 set VULKAN_INC=%VULKAN_SDK%\Include
-set EXTERNAL_INC=..\extern\volk
+set EXTERNAL_INC=%ROOT%..\extern\volk
 set VULKAN_LIBPATH=%VULKAN_SDK%\Lib
 set IGNORE_WARNINGS=-wd4127 -wd4706 -wd4100 -wd4996 -wd4505 -wd4201
 
-IF NOT EXIST ..\build mkdir ..\build
-pushd ..\build
+IF NOT EXIST %ROOT%..\build mkdir %ROOT%..\build
+pushd %ROOT%..\build
 del /Q/F/S *.* > nul
 
+REM -----------------------------
 REM Choose build type
-IF /I "%1"=="debug" (
+REM -----------------------------
+IF /I "%1"=="d" (
     echo Building DEBUG version...
     cl -MT -nologo -Od -Oi -Zi -FC -W3 /std:clatest /D_DEBUG %IGNORE_WARNINGS% ^
-        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c ^
-        /link %WIN32_LIBS% /DEBUG -incremental:no /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
+        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" "%ROOT%\app.c" "%ROOT%\win32.c" ^
+        /link %WIN32_LIBS% /DEBUG -incremental:no /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d_debug.exe
 
-popd
+    popd
     goto :EOF
 )
 
-IF /I "%1"=="release" (
+IF /I "%1"=="r" (
     echo Building RELEASE version...
     cl -MT -nologo -O2 -Oi -Os -GL -fp:fast /FC -W3 /std:clatest /DNDEBUG %IGNORE_WARNINGS% ^
-        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c ^
-        /link %WIN32_LIBS% /incremental:no /LTCG /OPT:REF /OPT:ICF /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
+        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" "%ROOT%\app.c" "%ROOT%\win32.c" ^
+        /link %WIN32_LIBS% /incremental:no /LTCG /OPT:REF /OPT:ICF /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d_release.exe
 
-popd
+    popd
     goto :EOF
 )
 
 echo Invalid option: %1
 echo Usage: %0 [debug|release]
+popd
 exit /b 1
 
+REM -----------------------------
+REM Error reporting
+REM -----------------------------
 IF %ERRORLEVEL% EQU 0 (
     echo Success!
 ) ELSE (
     echo Build failed with error %ERRORLEVEL%
 )
-
