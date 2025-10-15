aa3e5b2aa520101213a396bd145848f42635b4bc
Jonze123 <Balenor321@gmail.com>
2025-10-15 15:26:24 +0300

    Pass debug|release switch to build.bat

--- code/build.bat
+++ code/build.bat
@@ -1,5 +1,10 @@
 @echo off
-setlocal enabledelayedexpansion
+
+REM Check for argument
+IF "%1"=="" (
+    echo "Usage: %0 debug|release"
+    exit /b 1
+)
 
 set WIN32_LIBS=kernel32.lib user32.lib gdi32.lib
 set VULKAN_LIB=vulkan-1.lib
@@ -12,14 +17,30 @@ IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 del /Q/F/S *.* > nul
 
-REM DEBUG build
+REM Choose build type
+IF /I "%1"=="debug" (
+    echo Building DEBUG version...
+    cl -MT -nologo -Od -Oi -Zi -FC -W3 /std:clatest /D_DEBUG %IGNORE_WARNINGS% ^
+        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c ^
+        /link %WIN32_LIBS% /DEBUG -incremental:no /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
 
-REM cl -MT -nologo -Od -Oi -Zi -FC -W3 /std:clatest /D_DEBUG %IGNORE_WARNINGS%  -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c /link %WIN32_LIBS% /DEBUG -incremental:no /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
+popd
+    goto :EOF
+)
 
-REM RELEASE build
+IF /I "%1"=="release" (
+    echo Building RELEASE version...
+    cl -MT -nologo -O2 -Oi -Os -GL -fp:fast /FC -W3 /std:clatest /DNDEBUG %IGNORE_WARNINGS% ^
+        -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c ^
+        /link %WIN32_LIBS% /incremental:no /LTCG /OPT:REF /OPT:ICF /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
 
-cl -MT -nologo -O2 -Oi -Os -GL -fp:fast /FC -W3 /std:clatest /DNDEBUG %IGNORE_WARNINGS% -I "%VULKAN_INC%" -I "%EXTERNAL_INC%" ..\code\app.c ..\code\win32.c /link %WIN32_LIBS% /incremental:no /LTCG /OPT:REF /OPT:ICF /LIBPATH:"%VULKAN_LIBPATH%" /out:vulkan_3d.exe
+popd
+    goto :EOF
+)
 
+echo Invalid option: %1
+echo Usage: %0 [debug|release]
+exit /b 1
 
 IF %ERRORLEVEL% EQU 0 (
     echo Success!
@@ -27,5 +48,3 @@ IF %ERRORLEVEL% EQU 0 (
     echo Build failed with error %ERRORLEVEL%
 )
 
-popd
-endlocal
