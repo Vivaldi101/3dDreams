4dcae6e95336d657c6224e4cba29b895fffdb7e1
Jonze123 <Balenor321@gmail.com>
2025-11-03 19:21:46 +0200

    Continue with RT blas stuff

--- code/rt.c
+++ code/rt.c
@@ -8,33 +8,24 @@ static void rt_blas_build(arena scratch, vk_buffer_hash_table* buffer_hash, vk_g
    array_fixed(acceleration_geometries, VkAccelerationStructureGeometryKHR, geometry_count, scratch);
    array_fixed(build_infos, VkAccelerationStructureBuildGeometryInfoKHR, geometry_count, scratch);
 
-   vk_buffer* ib = buffer_hash_lookup(buffer_hash, ib_buffer_name);
-   vk_buffer* vb = buffer_hash_lookup(buffer_hash, vb_buffer_name);
-
    for(size i = 0; i < geometry_count; ++i)
    {
       vk_mesh_draw* draw = geometry->mesh_draws.data + i;
-      VkAccelerationStructureGeometryKHR* ag = acceleration_geometries.data + i;
-      VkAccelerationStructureBuildGeometryInfoKHR* bgi = build_infos.data + i;
 
-      array_add(primitive_count, (u32)draw->index_count / 3);   // triangles
+      VkAccelerationStructureGeometryKHR* ags = acceleration_geometries.data + i;
 
-      VkAccelerationStructureGeometryDataKHR triangle_geometry =
-      {VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_TRIANGLES_DATA_KHR};
+      // static assert the vertex format
+      static_assert(offsetof(vertex, vz) == offsetof(vertex, vx) + sizeof(float)*2);
 
-      // static assert the vertex formats
-      triangle_geometry.triangles.vertexFormat = VK_FORMAT_R32G32B32_SFLOAT;
-      //triangle_geometry.triangles.vertexData;
-      triangle_geometry.triangles.vertexStride = sizeof(vertex);
-      //triangle_geometry.triangles.maxVertex = (u32)(vb->size / sizeof(vertex));
-      triangle_geometry.triangles.indexType = VK_INDEX_TYPE_UINT32;
-      //triangle_geometry.triangles.indexData;
+      ags->sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_KHR;
+      ags->geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
+      ags->flags = VK_GEOMETRY_OPAQUE_BIT_KHR;
 
-      VkAccelerationStructureGeometryKHR acceleration_geometry =
-      {VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_KHR};
-      acceleration_geometry.geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
-      acceleration_geometry.flags = VK_GEOMETRY_OPAQUE_BIT_KHR;
-      acceleration_geometry.geometry = triangle_geometry;
+      ags->geometry.triangles.sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_TRIANGLES_DATA_KHR;
+      ags->geometry.triangles.vertexFormat = VK_FORMAT_R32G32B32_SFLOAT;
+      ags->geometry.triangles.vertexStride = sizeof(vertex);
+      ags->geometry.triangles.maxVertex = (u32)draw->vertex_count;
+      ags->geometry.triangles.indexType = VK_INDEX_TYPE_UINT32;
 
       VkAccelerationStructureBuildGeometryInfoKHR build_info =
       {VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_GEOMETRY_INFO_KHR};
@@ -42,11 +33,15 @@ static void rt_blas_build(arena scratch, vk_buffer_hash_table* buffer_hash, vk_g
       build_info.flags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR;
       build_info.mode = VK_BUILD_ACCELERATION_STRUCTURE_MODE_BUILD_KHR;
       build_info.geometryCount = 1;
-      build_info.pGeometries = ag;
+      build_info.pGeometries = ags;
 
       VkAccelerationStructureBuildSizesInfoKHR size_info =
       {VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_SIZES_INFO_KHR};
 
-      vkGetAccelerationStructureBuildSizesKHR(devices->logical, VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR, &build_info, primitive_count.data + i, &size_info);
+      array_add(primitive_count, (u32)draw->index_count / 3);   // triangles
+
+      vkGetAccelerationStructureBuildSizesKHR(devices->logical,
+                                              VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR,
+                                              &build_info, primitive_count.data + i, &size_info);
    }
 }
