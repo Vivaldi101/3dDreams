e46816245c0bd47b2d12cfbd32da4c448aced5ea
Jonze123 <Balenor321@gmail.com>
2025-12-07 14:25:41 +0200

    More free-list stuff

--- code/free_list.c
+++ code/free_list.c
@@ -1,35 +1,49 @@
 #include "free_list.h"
 
-static void release_list_node(list_node* n)
+static const size free_list_count = PAGE_SIZE / sizeof(list_node);
+
+static void list_node_release(list_node* n)
 {
-   n->next = n->free_list;
-   n->free_list = n;
+   n->next = global_free_list.nodes;
+   global_free_list.nodes = n;
+   global_free_list.count++;
 }
 
-static list_node* get_list_node(list_node* n)
+static list_node* list_node_get(list_node* h)
 {
    list_node* result = 0;
-   if(n->free_list)
-   {
-      result = n->free_list;
-      n->free_list = n->free_list->next;
 
+   // take from free list
+   if(global_free_list.nodes && global_free_list.count != 0)
+   {
+      result = global_free_list.nodes;
+      global_free_list.nodes = result->next;
+      global_free_list.count--;
       return result;
    }
 
-   result = push(n->a, list_node);
+   // make room in the free list
+   global_free_list.nodes = push(h->a, list_node, free_list_count, arena_persistent_kind);
+
+   result = global_free_list.nodes;
+   result->a = h->a;
+
+   h->next = result;
+
+   global_free_list.nodes = result->next;
+   global_free_list.count = free_list_count - 1;
 
    return result;
 }
 
-static void release_list(list_node* head)
+static void list_release(list* list)
 {
    list_node* t;
    do
    {
-      t = head->next;
-      release_list_node(head);
-      head = t;
+      t = list->head->next;
+      list_node_release(list->head);
+      list->head = t;
    }
    while(t);
 
--- code/free_list.h
+++ code/free_list.h
@@ -2,11 +2,25 @@
 
 align_struct list_node
 {
-   struct list_node* next;
-   struct list_node* free_list;
    arena* a;
+   struct list_node* next;
    void* data;
 } list_node;
 
+align_struct free_list
+{
+   size count;
+   list_node* nodes;
+} free_list;
+
+align_struct list
+{
+   free_list available_nodes;
+   list_node* nodes;
+   list_node* head;
+} list;
+
+static free_list global_free_list;
+
 #define list_node(T) __declspec(align(custom_alignment)) \
-struct { T* data; arena* a; struct list_node* next; struct list_node* free_list; }
+struct { arena* a; struct list_node* next; T* data; }
